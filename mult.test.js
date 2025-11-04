21813a55414781fb299f47a97cd273c6960e9c7a
DevJ25 <alavrezsanti@gmail.com>
2025-11-04 11:46:55 -0500

    actions multpiplicar v1

--- mult.test.js
+++ mult.test.js
@@ -3,7 +3,7 @@
 const { mult } = require("./mult");
 
 // Cambia 'jest' por 'test' o 'it'
-test('probarmltiplicacion 2+3 = 5', () => {
+test('probarmltiplicacion 2*2 = 4', () => {
 
   expect(mult(2, 2)).toBe(4);
   
